72394a62d8a36d60784acdb874a245ac0c3f327e
hauke.trumpf <hauke.trumpf@brille24.de>
2023-11-14 14:03:10 +0100

    newshit

--- src/components/mainContent.jsx
+++ src/components/mainContent.jsx
@@ -6,10 +6,10 @@ const MainContent = () => {
             <div class="m-auto">
                 <div className="grid md:grid-cols-3 gap-16">
                     <InView className="fade-in-left">
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg animate-fade-left">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg animate-fade-left bg-gray-600 text-white">
                             <div class="px-6 py-4">
                                 <div class="font-bold text-xl mb-2">test</div>
-                                <p class="text-gray-700 text-base">
+                                <p class="text-gray-700 text-base text-white">
                                     Lorem ipsum dolor sit amet, consectetur
                                     adipisicing elit. Voluptatibus quia, nulla!
                                     Maiores et perferendis eaque, exercitationem
@@ -20,10 +20,10 @@ const MainContent = () => {
                         </div>
                     </InView>
                     <InView className="fade-in-right">
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg animate-fade-right">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg animate-fade-right bg-gray-600 text-white">
                             <div class="px-6 py-4">
                                 <div class="font-bold text-xl mb-2">test</div>
-                                <p class="text-gray-700 text-base">
+                                <p class="text-gray-700 text-base text-white">
                                     Lorem ipsum dolor sit amet, consectetur
                                     adipisicing elit. Voluptatibus quia, nulla!
                                     Maiores et perferendis eaque, exercitationem
@@ -34,10 +34,10 @@ const MainContent = () => {
                         </div>
                     </InView>
                     <InView className="fade-in-left">
-                        <div class="max-w-sm rounded overflow-hidden shadow-lg animate-fade-left">
+                        <div class="max-w-sm rounded overflow-hidden shadow-lg animate-fade-left bg-gray-600 text-white">
                             <div class="px-6 py-4">
                                 <div class="font-bold text-xl mb-2">test</div>
-                                <p class="text-gray-700 text-base">
+                                <p class="text-gray-700 text-base text-white ">
                                     Lorem ipsum dolor sit amet, consectetur
                                     adipisicing elit. Voluptatibus quia, nulla!
                                     Maiores et perferendis eaque, exercitationem
